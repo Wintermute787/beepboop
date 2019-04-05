2964029db83b2078a3ab86f96109103a1ca46e9e
Joe Barnes <joebarnes333@gmail.com>
2019-04-05 15:38:19 -0700

    add functional logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,48 @@
 $(function () {
 
-    //I thought changing the int into an array of numbers would be helpful here, didn't end up using it//
-    function numberRange(int) {
-        var numArr = [];
-        for (var i = 0; i <= int; i++) {
-            numArr.push(i);
-        }
-        return (numArr);
-    }
+   var strArr = [];
+
+   function numToStr(int){
+       for(var i =0; i < int; i++){
+           strArr.push("" + i);
+           
+       }
+       return strArr;
+   }
+
+   function beepBoop(int){
+       num = '';
+       var word1 = "beep";
+       var word2 = "boop";
+       var word3 = "I'm Sorry";
+
+       for(var i = 0; i < strArr.length; i++){
+           num = strArr[i];
+           if(num.includes("3")){
+               strArr[i] = word3;
+           }else if(num.includes('2')){
+               strArr[i] = word2;
+           }else if(num.includes('1')){
+               strArr[i] = word1;
+           }
+       }
+       return strArr;
+   }
+
+   
 
     
-
-
-    //take user input through function//
-
-    function beepBoop(int) {
-        //create empty array//
-        var numArr = [];
-        //create words to push into array///
-        var word1 = "Beep";
-        var word2 = "Boop";
-        var word3 = "I'm Sorry Dave, I'm afraid I can't do that.";
-        //run through loop with the index from int//
-        for (var i = 0; i <= int; i++) {
-            //convert numArr to string. (this took me FOREVER to figure out)//
-            str = numArr.toString();
-            //continue pushing words into the string based on conditionals//
-            if (i % 3 === 0) {
-                numArr.push(word3);
-            } else if (str.includes("1")) {
-                numArr.push(word1);
-            } else if (str.includes("2")) {
-                numArr.push(word2);
-            } else {
-                numArr.push(i.toString());
-            }
-        }
-        //return the final numArr string//
-        return numArr;
-    }
-
     
-
 
     $('#boop-form').submit(function (event) {
         event.preventDefault();
         var number = parseInt($('input#boop').val());
-        
-        $("#boop-text").text("<li>" + beepBoop(number) + "</li>");
+
+      
+        strArr =[];
+        numToStr(number);
+        beepBoop(number);
+        $("#boop-text").text(beepBoop(number));
 
 
     });
